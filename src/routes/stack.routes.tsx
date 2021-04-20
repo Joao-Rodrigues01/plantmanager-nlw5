444c5144a8668177e930d3c420015bace217af39
João Victor <j2003.victir@gmail.com>
2021-04-20 15:44:05 -0300

    create stack routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Wellcome } from '../pages/Wellcome';
+import { UserIdentification } from '../pages/UserIdentification';
+import { Confirmation } from '../pages/Confirmation';
+
+import colors from '../styles/colors';
+
+const stackRoutes = createStackNavigator(); 
+
+const AppRoutes: React.FC = () => (
+  <stackRoutes.Navigator 
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: colors.white
+      },
+    }}
+  >
+    <stackRoutes.Screen 
+      name="Wellcome"
+      component={Wellcome}
+    />
+
+    <stackRoutes.Screen 
+      name="UserIdentification"
+      component={UserIdentification}
+    />
+
+    <stackRoutes.Screen 
+      name="Confirmation"
+      component={Confirmation}
+    />
+  </stackRoutes.Navigator>
+)
+
+export default AppRoutes;
