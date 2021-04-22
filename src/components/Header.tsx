cf5d7a7e27153cdb9a44b6acafb34dbcbb136636
João Victor <j2003.victir@gmail.com>
2021-04-22 12:51:56 -0300

    add haeder component

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/FotoPerfil.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Jonas</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image}/>
+    </View>
+  )
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: getStatusBarHeight(),
+    paddingVertical: 20
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
+  },
+});
